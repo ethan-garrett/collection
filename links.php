58288407029f182eb3e22ab19a98532b161e81da
Ethan Garrett <garrettethan63@gmail.com>
2022-02-27 11:04:03 +0000

    Began overhauling website with more content

---
+++ links.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Classic Tetris Elo Rankings</title>
+    <link href="normalize.css" type="text/css" rel="stylesheet"/>
+    <link href="indexstyling.css" type="text/css" rel="stylesheet"/>
+    <meta name="viewport" content="width=device-width">
+</head>
+<body>
+<header>
+    <a href="index.php">
+
+        <p class="title">CLASSIC</p>
+        <p class="title">TETRIS</p>
+        <p class="title">ELO</p>
+        <p class="title">RANKINGS</p>
+    </a>
+</header>
+<div class="infoDiv">
+    <p>Click the title to head to the Homepage</p>
+    <p>Note: This website is a WIP and may have inaccurate data</p>
+</div>
+
+<section class="entrySection">
+    <div class="infoDiv">
+   
+    </div>
+</section>
+</body>
+</html>
